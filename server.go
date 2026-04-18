3c856d51c3aa2fcdcac9aa60f0f9290b1a1eb98a
Lucas Galvão Janot <113521855+lucasgjanot@users.noreply.github.com>
2026-04-18 23:38:55 +0000

    .

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log"
 	"net"
 	"net/http"
 	"os"
@@ -47,6 +48,7 @@ func (s *server) start() error {
 	if err != nil {
 		return err
 	}
+	log.Printf("Linko is running on http://localhost:%d", ln.Addr().(*net.TCPAddr))
 	if err := s.httpServer.Serve(ln); !errors.Is(err, http.ErrServerClosed) {
 		return err
 	}
